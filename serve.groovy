64a703510fd5bf111a8c1b7092082742b77ff344
Cedric Champeau <cedric.champeau@gmail.com>
2014-02-03 23:04:17 +0100

    Added a new blog post about the JBake toolchain

--- serve.groovy
+++ serve.groovy
@@ -22,7 +22,7 @@ def resource_handler = new ResourceHandler()
 resource_handler.directoriesListed = true
 resource_handler.welcomeFiles = ['index.html']
 
-resource_handler.resourceBase = 'output'
+resource_handler.resourceBase = 'build/jbake'
 
 def context = new ContextHandler()
 context.handler = resource_handler
